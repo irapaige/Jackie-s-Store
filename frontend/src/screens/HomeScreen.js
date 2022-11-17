5b7e0a17212a2a45374d6091a8cc621232bad7ea
irapaige <38634811+irapaige@users.noreply.github.com>
2022-11-16 23:29:47 -0500

    added context

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,12 @@
 import { useEffect, useReducer, useState } from 'react';
 
 import axios from 'axios';
-
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../components/Product';
-// import data from '../data';
+import { Helmet } from 'react-helmet-async';
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -41,12 +42,15 @@ function HomeScreen() {
     }, []);
     return (
         <div>
+            <Helmet>
+                <title>Jackie's Store</title>
+            </Helmet>
             <h1>Featured Products</h1>
             <div className="products">
                 {loading ? (
-                    <div>Loading...</div>
+                    <LoadingBox />
                 ) : error ? (
-                    <div>{error}</div>
+                    <MessageBox variant="danger">{error}</MessageBox>
                 ) : (
                                        <Row>
                     {products.map((product) => (
