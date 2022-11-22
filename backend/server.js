6721aa29a90065dce0d5b5204e000a266d990188
irapaige <38634811+irapaige@users.noreply.github.com>
2022-11-21 21:33:49 -0500

    fixed user email and password

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 // import data from './data.js';
+import userRouter from './routes/userRoutes.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from "./routes/seedRoutes.js";
@@ -16,11 +17,18 @@ mongoose
         console.log(err.message);
     });
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/api/seed',seedRouter);
 app.use('/api/products',productRouter)
 
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+app.use((err, req, res) => {
+    res.status(500).send({ message: err.message });
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`serve at http://localhost:${port}`);
