500ccbece4cf5d0a38e960cec64e93a39e60866f
irapaige <38634811+irapaige@users.noreply.github.com>
2022-12-16 20:41:55 -0500

    changed img to process env

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -3,31 +3,33 @@ import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 import Rating from './Rating';
 
-
 function Product(props) {
-    const { product } = props;
+  const { product } = props;
 
-
-    return (
-        <Card>
-            <Link to={`/product/${product.slug}`}>
-                <img src={product.image} className="card-img-top" alt={product.name} />
-            </Link>
-            <Card.Body>
-                <Link to={`/product/${product.slug}`}>
-                    <Card.Title>{product.name}</Card.Title>
-                </Link>
-                <Rating rating={product.rating} numReviews={product.numReviews} />
-                <Card.Text>${product.price}</Card.Text>
-                {product.countInStock === 0 ? (
-                    <Button variant="light" disabled>
-                        Out of stock
-                    </Button>
-                ) : (
-                    <Button id={'in-stock'}>In Stock</Button>
-                )}
-            </Card.Body>
-        </Card>
-    )
+  return (
+    <Card>
+      <Link to={`/product/${product.slug}`}>
+        <img
+          src={process.env.PUBLIC_URL + product.image}
+          className='card-img-top'
+          alt={product.name}
+        />
+      </Link>
+      <Card.Body>
+        <Link to={`/product/${product.slug}`}>
+          <Card.Title>{product.name}</Card.Title>
+        </Link>
+        <Rating rating={product.rating} numReviews={product.numReviews} />
+        <Card.Text>${product.price}</Card.Text>
+        {product.countInStock === 0 ? (
+          <Button variant='light' disabled>
+            Out of stock
+          </Button>
+        ) : (
+          <Button id={'in-stock'}>In Stock</Button>
+        )}
+      </Card.Body>
+    </Card>
+  );
 }
 export default Product;
